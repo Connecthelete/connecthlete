dc84a18459b53d590ca0413d464ee2fc481e2122
Adam Ozdemir <adamozdemir@Adams-MacBook-Pro.local>
2022-10-16 03:16:45 -0400

    indent bug fix

--- app/src/main/java/com/example/connecthlete/MainActivity.java
+++ app/src/main/java/com/example/connecthlete/MainActivity.java
@@ -2,13 +2,48 @@ package com.example.connecthlete;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button goAct3;
+    private Button goSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this,"firebase connection really worked", Toast.LENGTH_LONG).show();
+
+        goSignUp=(Button) findViewById(R.id.signup);
+        goSignUp.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent=new Intent(MainActivity.this,SignUp.class);
+                startActivity(intent);
+                finish();
+            }
+
+
+        });
+        goAct3=(Button) findViewById(R.id.signin);
+        goAct3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent=new Intent(MainActivity.this,FindPeople.class);
+               //check if username is unique
+                startActivity(intent);
+                finish();
+            }
+
+
+        });
+
+
     }
+
+
 }
---
+++ app/src/main/java/com/example/connecthlete/SignUp.java
@@ -0,0 +1,32 @@
+package com.example.connecthlete;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SignUp extends AppCompatActivity {
+   // private Button goSignin=(Button) findViewById(R.id.signupbuttonid);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+//        goSignin.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v){
+//                Intent intent=new Intent(SignUp.this,MainActivity.class);
+//                startActivity(intent);
+//                finish();
+//            }
+//
+//
+//        });
+
+
+
+
+    }
+}
